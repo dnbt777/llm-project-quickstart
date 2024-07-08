aa73b4a869088d79a3beb37615ef8415af06e613
dnbt777 <dnbt539@gmail.com>
2024-07-08 03:50:15 -0700

    Updated example.py

--- example.py
+++ example.py
@@ -9,9 +9,20 @@ modelinterface = ModelInterface()
 prompt = "Introduce yourself, with enthusiasm! Keep it short though and use emojis!"
 
 if all([aws_region, access_key, secret_key, aws_username]):
+    for model in "haiku sonnet opus sonnet3.5".split():
+        model = "bedrock-" + model
+        try:
+            response = modelinterface.send_to_ai(prompt, model=model)
+            modelinterface.cost_tracker.show_cost_data()
+        except:
+            print("Test failed for model " + model)
     for model in "haiku sonnet opus".split():
-        response = modelinterface.send_to_ai(prompt, model=model)
-        modelinterface.cost_tracker.show_cost_data()
+        model = "anthropic-" + model
+        try:
+            response = modelinterface.send_to_ai(prompt, model=model)
+            modelinterface.cost_tracker.show_cost_data()
+        except:
+            print("Test failed for model" + model)
 
 if openai_api_key:
     for model in "gpt-4o gpt-3.5-turbo".split():
